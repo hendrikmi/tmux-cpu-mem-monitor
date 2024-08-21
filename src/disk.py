12fbdcda8d4349b79c68c818dbb2c6cd4dbece66
Elias Ericsson-Rydberg <elias.ericsson.rydberg@gmail.com>
2024-08-21 21:38:14 +0200

    Make human readable and add the option to show free space

--- src/disk.py
+++ src/disk.py
@@ -1,5 +1,6 @@
 import argparse
 import psutil
+from psutil._common import bytes2human
 
 def get_disk_usage_percent():
     """Display disk usage as a percentage"""
@@ -7,23 +8,29 @@ def get_disk_usage_percent():
     disk_usage = disk.percent
     return f"{disk_usage}%"
 
+def get_disk_usage_free():
+    """Display free disk in GB"""
+    disk = psutil.disk_usage("/")
+    return f"{bytes2human(disk.free)}"
+
 def get_disk_usage_total():
     """Display disk usage as used/total in GB"""
     disk = psutil.disk_usage("/")
-    available_gb = disk.free / (1024**3)  # Convert to GB
-    total_gb = disk.total / (1024**3)
-    used_gb = total_gb - available_gb
-    return f"{used_gb:.2f}GB/{total_gb:.2f}GB"
+    return f"{bytes2human(disk.used)}/{bytes2human(disk.total)}"
 
 def main(args):
     if args.total:
         disk_usage = get_disk_usage_total()
+    elif args.free:
+        disk_usage = get_disk_usage_free()
     else:
         disk_usage = get_disk_usage_percent()
+    
     print(disk_usage)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--total", action="store_true", default=False)
+    parser.add_argument("-f", "--free", action="store_true", default=False)
     args = parser.parse_args()
     main(args)

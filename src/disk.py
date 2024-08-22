48cbe8bd0c1f7237cafb88e7349c9054a1775a66
Elias Ericsson-Rydberg <elias.ericsson.rydberg@gmail.com>
2024-08-22 22:19:44 +0200

    Code formatting done with Ruff

--- src/disk.py
+++ src/disk.py
@@ -2,21 +2,25 @@ import argparse
 import psutil
 from psutil._common import bytes2human
 
+
 def get_disk_usage_percent():
     """Display disk usage as a percentage"""
     disk = psutil.disk_usage("/")
     disk_usage = disk.percent
     return f"{disk_usage}%"
 
+
 def get_disk_usage_free():
     """Display free disk in GB"""
     disk = psutil.disk_usage("/")
     return f"{bytes2human(disk.free)}"
 
+
 def get_disk_usage_total():
     """Display disk usage as used/total in GB"""
     disk = psutil.disk_usage("/")
     return f"{bytes2human(disk.used)}/{bytes2human(disk.total)}"
+
 
 def main(args):
     if args.total:
@@ -25,8 +29,9 @@ def main(args):
         disk_usage = get_disk_usage_free()
     else:
         disk_usage = get_disk_usage_percent()
-    
+
     print(disk_usage)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()

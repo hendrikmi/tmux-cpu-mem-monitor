84f16549a9b2d24266c35373d15be1ef88887a32
Elias Ericsson-Rydberg <elias.ericsson.rydberg@gmail.com>
2024-08-31 15:02:05 +0200

    Remove unused function

--- src/disk.py
+++ src/disk.py
@@ -5,10 +5,6 @@ import sys
 from psutil._common import bytes2human
 
 
-def unused_function():
-    return "Hello"
-
-
 def _get_default_path():
     """Get the default path for the current platform"""
     if sys.platform == "win32":

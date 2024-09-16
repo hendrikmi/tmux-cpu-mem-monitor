637876fa1a2da1fead40d2c50b7d05a1619ef57d
Elias Ericsson-Rydberg <elias.ericsson.rydberg@gmail.com>
2024-09-16 11:32:00 +0300

    Add test for humanoid battery counter

--- src/battery.py
+++ src/battery.py
@@ -31,9 +31,41 @@ def get_battery_time():
     return f"{hours}h {minutes}m"
 
 
+def get_battery_human_readable():
+    """Display the remaining battery amount in a fun human-readable format.
+
+    Examples:
+    - Fully charged
+    - Almost full
+    - More than half full
+    ...
+    """
+    if _get_charging_status():
+        return "Charging"
+
+    battery = psutil.sensors_battery().percent
+    if battery == 100:
+        return "Fully charged"
+    elif battery >= 95:
+        return "Almost full"
+    elif battery >= 75:
+        return "More than half full"
+    elif battery == 50:
+        return "Half full"
+    elif battery <= 25:
+        return "Less than half full"
+    elif battery <= 5:
+        return "Battery is almost empty"
+    elif battery == 1:
+        return "I'm dying over here!"
+    return "Battery is empty"
+
+
 def main(args):
     if args.time:
         battery = get_battery_time()
+    elif args.human_readable:
+        battery = get_battery_human_readable()
     else:
         battery = get_battery_percent()
 
@@ -45,5 +77,6 @@ if __name__ == "__main__":
     group = parser.add_mutually_exclusive_group()
     group.add_argument("-p", "--percent", action="store_true", default=False)
     group.add_argument("-t", "--time", action="store_true", default=False)
+    group.add_argument("-hr", "--human-readable", action="store_true", default=False)
     args = parser.parse_args()
     main(args)
